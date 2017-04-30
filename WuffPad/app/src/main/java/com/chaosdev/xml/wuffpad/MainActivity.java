d41dbe5835c9bfecbdf0fa5224d1ca5a6ce41689
Olfi01 <flom@meyer-alpers.de>
2017-04-30 12:27:59 +0200

    Startup commits
    VCS didn't seem to do what I wanted, so here we go with github desktop

---
+++ WuffPad/app/src/main/java/com/chaosdev/xml/wuffpad/MainActivity.java
@@ -0,0 +1,13 @@
+package com.chaosdev.xml.wuffpad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
